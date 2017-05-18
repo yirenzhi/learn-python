235c906d0c05a3546df09f1dfb6cf6fdfcb3db95
yirenzhi <yiren_zhi@163.com>
2017-05-18 18:04:19 +0800

    廖雪峰web教程1-2.5天

---
+++ pyWebApp/awesome-py3web/www/orm.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+
+'''
+创建连接池
+
+我们需要创建一个全局的连接池，每个HTTP请求都可以从连接池中直接获取数据库连接。使用连接池的好处是不必频繁地打开和关闭数据库连接，而是能复用就尽量复用。
+
+连接池由全局变量__pool存储，缺省情况下将编码设置为utf8，自动提交事务：
+'''
+
+@asyncio.coroutine
+def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host = kw.get('host','localhost'),
+        port = kw.get('',3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf8'),
+        autocommit = kw.get('maxsize',10),
+        minsize = kw.get('minsize',1),
+        loop = loop
+    
+    )
+    
+    
+@asyncio.coroutine
+def select(sql.args.size=None):
+    log(sql,args)
+    global __pool
+    with(yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?','%s',args or ()))
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned:%s' % len(rs))
+        return rs
+    
+    
+    
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with(yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?','%s'),args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+    
+    
+    
+    
